d0117ab6c46b29d378b3e7cd8e438248ebd84308
Tyler Herbert <Tyeherb94@gmail.com>
2022-02-17 20:57:05 -0500

    FIRST COMMIT

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 // TODO: Declare any global variables we need
+let headRoll = 0
+let tailRoll = 0
+
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -6,16 +10,62 @@ document.addEventListener('DOMContentLoaded', function () {
     // You can remove it once you see it in your browser console in the developer tools
     console.log('Hi')
 
+
     // TODO: Add event listener and handler for flip and clear buttons
 
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
+// GET REQUIRED ELEMENTS
+
+const flipBtn = document.getElementById('flip')
+const pennyImage= document.getElementById('penny-image')
+const message = document.getElementById('message')
+const heads= document.getElementById("heads")
+const tails= document.getElementById("tails")
+const headsPerc= document.getElementById("heads-percent")
+const tailsPerc= document.getElementById("tails-percent")
+
+flipBtn.addEventListener('click',()=>{
+    let flippedHeads = Math.random()<0.5
+
+    if (flippedHeads){
+    pennyImage.src= 'assets/images/penny-heads.jpg'
+    message.textContent = "YOU FLIPPED HEADS"
+    headRoll++
+     } else{
+         pennyImage.src="assets/images/penny-tails.jpg"
+         message.textContent= "YOU FLIPPED TAILS"
+         tailRoll++
+
+     }
+
+
+     heads.textContent = headRoll
+     tails.textContent = tailRoll
+
+
+
 
         // Update the scorboard
+
         // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
+        let total= tailRoll + headRoll
+        let percentHeads =0
+        let percentTails=0
+    if (total > 0){
+
+        percentHeads = Math.round((headRoll / total)*100)
+        percentTails = Math.round((tailRoll / total)*100)
+    }
+       
+        headsPerc.textContent= percentHeads + '%'
+        tailsPerc.textContent= percentTails + '%'
+
+})
+
+    
+
+        
+
+        
         // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
         // TODO: Update the display of each table cell
 
